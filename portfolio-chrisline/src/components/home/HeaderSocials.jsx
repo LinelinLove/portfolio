420b899c2092729d3ca0268dcfeb338ce3ac7570
Linelinlove <chrisline.lin@outlook.fr>
2023-08-28 18:22:12 +0200

    sidebar and home components

---
+++ portfolio-chrisline/src/components/home/HeaderSocials.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+    return (
+        <div className="home__socials">
+            <a href="https://www.linkedin.com/in/chrisline-lin-284652185/" className="home__social-link" target="_blank">
+                <i class="fa-brands fa-linkedin"></i>
+            </a>
+
+            <a href="https://github.com/LinelinLove" className="home__social-link" target="_blank">
+                <i class="fa-brands fa-github"></i>
+            </a>
+
+            {/* <a href="" className="home__social-link" target="_blank">
+                <i class="fa-brands fa-behance"></i>
+            </a>
+
+            <a href="" className="home__social-link" target="_blank">
+                <i class="fa-brands fa-dribbble"></i>
+            </a>
+
+            <a href="" className="home__social-link" target="_blank">
+                <i class="fa-brands fa-pinterest"></i>
+            </a> */}
+        </div>
+    )
+}
+
+export default HeaderSocials
--- portfolio-chrisline/src/components/home/Home.jsx
+++ portfolio-chrisline/src/components/home/Home.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import "./home.css";
+import Me from "../../assets/avatar-1.svg";
+import HeaderSocials from './HeaderSocials';
+import ScrollDown from './ScrollDown';
+import Shapes from './Shapes';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="home container" id="home">
+      <div className="intro">
+        <img src={Me} alt="avatar" className="home__img" />
+        <h1 className="home__name">Chrisline Lin</h1>
+        <span className="home__education">I'm a Full-stack developper</span>
+
+        <HeaderSocials />
+
+        <a href="#contact" className="btn">Contact me</a>
+
+        <ScrollDown />
+      </div>
+
+      <Shapes />
+    </section>
   )
 }
 
