eaf80f9a8e67f3d8a4b0d6fa6fff5d5d990ef6ef
Linelinlove <chrisline.lin@outlook.fr>
2023-08-30 19:18:37 +0200

    send mail

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import "./contact.css";
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const [data, setData] = useState({ name: "", email: "", phone: "", subject: "", message: "" });
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setData({ ...data, [name]: value })
+  }
+
+  const form = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_03v97sa', 'template_u8ogcmd', form.current, 'ZPy_vjHYaXjiYGYSF')
+      .then((result) => {
+        console.log(result.text);
+        console.log("message sent");
+        // setTimeout(() => {
+        //   e.target.reset();
+        // });
+        setData({
+          name: "",
+          email: "",
+          phone: "",
+          subject: "",
+          message: ""
+        });
+
+      }, (error) => {
+        console.log(error.text);
+      });
+
+  }
   return (
     <section className="contact container section" id="contact">
       <h2 className="section__title">Get In Touch</h2>
@@ -10,32 +43,35 @@ const Contact = () => {
         <div className="contact__info">
           <h3 className="contact__title">Let's talk about everything!</h3>
           <p className="contact__details">Don't like forms? Send me an email. 👋</p>
+          <p><a href="mailto:chrisline.lin@outlook.fr">chrisline.lin@outlook.fr</a></p>
         </div>
 
-      <form action="" className="contact__form">
-        <div className="contact__form-group">
+        <form ref={form} method='post' onSubmit={handleSubmit} action="" className="contact__form">
+          <div className="contact__form-group">
 
-          <div className="contact__form-div">
-            <input type="text" className="contact__form-input" placeholder='Insert your name' />
+            <div className="contact__form-div">
+              <input type="text" name='name' onChange={handleChange} value={data.name} className="contact__form-input" placeholder='Insert your name' />
+            </div>
+
+            <div className="contact__form-div">
+              <input type="email" name='email' onChange={handleChange} value={data.email} className="contact__form-input" placeholder='Insert your email' />
+            </div>
           </div>
 
           <div className="contact__form-div">
-            <input type="email" className="contact__form-input" placeholder='Insert your email' />
+            <input type="tel" name='phone' onChange={handleChange} value={data.phone} className="contact__form-input" placeholder='Insert your phone' />
           </div>
-        </div>
-        
-        <div className="contact__form-div">
-          <input type="text" className="contact__form-input" placeholder='Insert your subject' />
-        </div>
 
-        <div className="contact__form-div contact__form-area">
-          <textarea name="" id="" cols="30" rows="10" className='contact__form-input' placeholder='Write your message'></textarea>
-        </div>
+          <div className="contact__form-div">
+            <input type="text" name='subject' onChange={handleChange} value={data.subject} className="contact__form-input" placeholder='Insert your subject' />
+          </div>
 
-        <button className="btn">Send Message</button>
+          <div className="contact__form-div contact__form-area">
+            <textarea name="message" onChange={handleChange} value={data.message} id="" cols="30" rows="10" className='contact__form-input' placeholder='Write your message'></textarea>
+          </div>
 
-
-      </form>
+          <button type='submit' className="btn">Send Message</button>
+        </form>
       </div>
 
 
